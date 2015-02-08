bfbdb7b1500e14ba975b0161908381f1ed6b778b
marshl <leeism@live.com.au>
2015-02-08 20:25:15 +1100

    Added Fate Reforged

--- defines.php
+++ defines.php
@@ -160,6 +160,7 @@ Defines::$SetcodeOrder = array(
 	'DDN',
 	'KTK',
 	'C14',
+	'FRF',
 );
 
 Defines::$SetcodeToIndices = array_flip(Defines::$SetcodeOrder);
@@ -299,12 +300,13 @@ Defines::$SetCodeToNameMap = array(
 	'DDN' => "Duel Decks: Speed vs. Cunning",
 	'KTK' => "Khans of Tarkir",
 	'C14' => "Commander 2014",
+	'FRF' => "Fate Reforged",
 );
 
 Defines::$SetNameToCodeMap = array_flip(Defines::$SetCodeToNameMap);
 
 Defines::$CardBlocksToSetCodes = array(
-	"Khans Block"				=> array( 'KTK', ),
+	"Khans Block"				=> array( 'KTK', 'FRF', ),
 	"Theros Block"				=> array( 'THS', 'BNG', 'JOU', ),
 	"Return to Ravnica Block"	=> array( 'RTR', 'GTC', 'DGM', ),
 	"Innistrad Block"			=> array( 'ISD', 'DKA', 'AVR', ),
@@ -338,9 +340,9 @@ Defines::$CardBlocksToSetCodes = array(
 
 
 Defines::$CardFormats = array(
-	"Standard"					=> array( 'THS', 'BNG', 'JOU', 'M15', 'KTK', ),
-	"Extended"					=> array("SOM", "MBS", "NPH", "M12", "ISD", "DKA", "AVR", "M13", "RTR", "GTC", 'DGM', 'M14', 'THS', 'BNG', 'JOU', 'M15', ),
-	"Modern"					=> array("8ED", "MRD", "DST", "5DN", "CHK", "BOK", "SOK", "9ED", "RAV", "GPT", "DIS", "CSP", "TSP", "TSB", "PLC", "FUT", "10E", "LRW", "MOR", "SHM", "EVE", "ALA", "CON", "ARB", "M10", "ZEN", "WWK", "ROE", "M11", "SOM", "MBS", "NPH", "M12", "ISD", "DKA", "AVR", "M13", "RTR", "GTC", 'DGM', 'M14', 'THS', 'BNG', 'JOU', 'M15', 'KTK', ),
+	"Standard"					=> array( 'THS', 'BNG', 'JOU', 'M15', 'KTK', 'FRF', ),
+	"Extended"					=> array("SOM", "MBS", "NPH", "M12", "ISD", "DKA", "AVR", "M13", "RTR", "GTC", 'DGM', 'M14', 'THS', 'BNG', 'JOU', 'M15', 'KTK', 'FRF', ),
+	"Modern"					=> array("8ED", "MRD", "DST", "5DN", "CHK", "BOK", "SOK", "9ED", "RAV", "GPT", "DIS", "CSP", "TSP", "TSB", "PLC", "FUT", "10E", "LRW", "MOR", "SHM", "EVE", "ALA", "CON", "ARB", "M10", "ZEN", "WWK", "ROE", "M11", "SOM", "MBS", "NPH", "M12", "ISD", "DKA", "AVR", "M13", "RTR", "GTC", 'DGM', 'M14', 'THS', 'BNG', 'JOU', 'M15', 'KTK', 'FRF', ),
 	"Core Sets"					=> array('LEA', 'LEB', '2ED', '3ED', '4ED', '5ED', '6ED', '7ED', '8ED', '9ED', '10E', 'M10', 'M11', 'M12', 'M13', 'M14', 'M15', ),
 	"Ice Age Block"				=> array("ICE", "ALL", "CSP" ),
 	"Innistrad Block"			=> array("ISD", "DKA", "AVR" ),
